8a973730b691d942b8b3b5fa020085487ff7a331
claytonfortunato <claytonreis10@gmail.com>
2023-03-02 14:38:52 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Private from "./pages/Private";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Sistema de Login</h1>
+        <nav></nav>
+      </header>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
