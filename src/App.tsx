097e9cb571b77142b9f1fbf8a527c8bad7297250
claytonfortunato <claytonreis10@gmail.com>
2023-03-02 15:06:40 -0300

    feat: router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Private from "./pages/Private";
+import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Sistema de Login</h1>
-        <nav></nav>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Página Privada</Link>
+        </nav>
       </header>
       <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<Private />} />
+      </Routes>
     </div>
   );
 }
