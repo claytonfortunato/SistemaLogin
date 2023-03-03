9f4dc1bab23ee49e928be76012230216aa694830
claytonfortunato <claytonreis10@gmail.com>
2023-03-02 21:35:06 -0300

    feat: api e context api

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { User } from "../../types/User";
+import { useApi } from "../../hook/useApi";
 
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const [user, SetUser] = useState<User | null>(null);
+  const api = useApi();
 
-  const signin = (email: string, password: string) => {};
+  const signin = async (email: string, password: string) => {
+    const data = await api.signin(email, password);
+    if (data.user && data.token) {
+      SetUser(data.user);
+      return true;
+    }
+    return false;
+  };
 
-  const singout = () => {};
+  const singout = async () => {
+    await api.logout();
+    SetUser(null);
+  };
 
   return (
     <AuthContext.Provider value={{ user, signin, singout }}>
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import { GlobalStyle } from "./GlobalStyle";
 
+import { AuthProvider } from "./contexts/Auth/AuthProvider";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+        <GlobalStyle />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
