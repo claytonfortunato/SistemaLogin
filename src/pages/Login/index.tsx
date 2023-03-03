92a8e79688c2a265eaf853e03c636e905fac6cd4
claytonfortunato <claytonreis10@gmail.com>
2023-03-02 23:07:56 -0300

    feat: page de login e token

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { ChangeEvent, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
+
 import * as C from "./styles";
 
 const Login = () => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClick = () => {};
+  const handleEmailInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event?.target.value);
+  };
+
+  const handleClick = async () => {
+    if (email && password) {
+      const isLogged = await auth.signin(email, password);
+      if (isLogged) {
+        navigate("/");
+      } else {
+        alert("Não deu certo");
+      }
+    }
+  };
 
   return (
     <C.Container>
       <h2>Página Fechada</h2>
 
-      <input type="text" value={email} placeholder="Digite seu e-mail" />
-      <input type="password" value={password} placeholder="Digite sua senha" />
+      <input
+        type="text"
+        value={email}
+        onChange={handleEmailInput}
+        placeholder="Digite seu e-mail"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={handlePasswordInput}
+        placeholder="Digite sua senha"
+      />
 
       <button onClick={handleClick}>Logar</button>
     </C.Container>
